143c9032ff53fbbebb69bb3819a8c663f0148c75
Anton Lidström <antonlidstroem@outlook.com>
2026-01-14 14:34:20 +0100

    Layout changes

---
+++ BudgetPlanner.WPF/Views/TransactionListView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace BudgetPlanner.WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for TransactionListView.xaml
+    /// </summary>
+    public partial class TransactionListView : UserControl
+    {
+        public TransactionListView()
+        {
+            InitializeComponent();
+        }
+    }
+}
